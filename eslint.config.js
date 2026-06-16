d41f52071a0888b26acf8923097c042756623d5f
Pannuvia Soares Monteiro <pannuvia@hotmail.com>
2026-06-15 22:33:14 -0300

    ci: adicionando os arquivos eslint.config.js e pipeline.yaml

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      },
+    },
+    rules: {
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-unused-vars': 'warn',
+      'no-console': 'off', // Set to 'warn' or 'error' for production apps
+    },
+  },
+  {
+    ignores: ['mochawesome-report/'],
+  },
+];
